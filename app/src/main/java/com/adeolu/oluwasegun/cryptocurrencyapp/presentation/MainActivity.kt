cfd21623aea4c5ed12e5160e3bdfa69eeaadb836
Oluwasegun <adeolusegun99@gmail.com>
2022-04-16 22:56:20 +0100

    setting up the different packages to show and follow clean architecture

---
+++ app/src/main/java/com/adeolu/oluwasegun/cryptocurrencyapp/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.adeolu.oluwasegun.cryptocurrencyapp.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.adeolu.oluwasegun.cryptocurrencyapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
